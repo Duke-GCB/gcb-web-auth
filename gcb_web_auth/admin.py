c24c5b9c0a1a3f9619e52887437648080c8d865d
John Bradley <johnbradley2008@gmail.com>
2017-07-17 15:17:32 -0400

    add group manager setup to admin

--- gcb_web_auth/admin.py
+++ gcb_web_auth/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from gcb_web_auth.models import OAuthService, OAuthToken, DukeDSAPIToken
+from gcb_web_auth.models import OAuthService, OAuthToken, DukeDSAPIToken, GroupManagerConnection
 
 # Register your models here.
 admin.site.register(OAuthService)
 admin.site.register(OAuthToken)
 admin.site.register(DukeDSAPIToken)
+admin.site.register(GroupManagerConnection)
