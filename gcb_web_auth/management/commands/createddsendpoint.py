c46c26d3354f649c9d1354dd4349f99c3ae4cab2
Dan Leehr <dan.leehr@duke.edu>
2018-04-10 14:54:10 -0400

    Fix createddsendpoint command to work

--- gcb_web_auth/management/commands/createddsendpoint.py
+++ gcb_web_auth/management/commands/createddsendpoint.py
@@ -8,16 +8,16 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('name', type=str, help='Endpoint Name')
         parser.add_argument('api_root', type=str, help='DukeDS API url')
-        parser.add_argument('agent_key', type=str, help='Agent key to use when authenticating this application')
         parser.add_argument('portal_root', type=str, help='DukeDS portal url')
+        parser.add_argument('agent_key', type=str, help='Agent key to use when authenticating this application')
         parser.add_argument('openid_provider_id', type=str, help='OpenID provider from DukeDS api/v1/auth_providers')
 
     def handle(self, *args, **options):
-        name = options['name']
-
-        try:
-            endpoint = DDSEndpoint.objects.get(name=name)
-            endpoint.update(**options)
-            endpoint.save()
-        except DDSEndpoint.DoesNotExist:
-            endpoint = DDSEndpoint.objects.create(**options)
+        fields = dict()
+        for k in {'name','api_root','portal_root','agent_key','openid_provider_id'}:
+            fields[k] = options[k]
+        existing = DDSEndpoint.objects.filter(name=fields['name'])
+        if existing:
+            existing.update(**fields)
+        else:
+            DDSEndpoint.objects.create(**fields)
