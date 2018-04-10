c12f3d864a01158ff1b8b5e7441732aabbfc44b8
Dan Leehr <dan.leehr@duke.edu>
2018-04-10 12:09:53 -0400

    Update DDSSettings references to DDSEndpoint

--- gcb_web_auth/management/commands/createddssettings.py
+++ gcb_web_auth/management/commands/createddssettings.py
@@ -1,24 +1,23 @@
 from django.core.management.base import BaseCommand
-from gcb_web_auth.models import DukeDSSettings
+from gcb_web_auth.models import DDSEndpoint
 
 
 class Command(BaseCommand):
-    help = 'Create/Replace DukeDSSettings in the database.'
+    help = 'Create/Replace DDSEndpoint in the database.'
 
     def add_arguments(self, parser):
-        parser.add_argument('url', type=str, help='DukeDS API url')
+        parser.add_argument('name', type=str, help='Endpoint Name')
+        parser.add_argument('api_root', type=str, help='DukeDS API url')
+        parser.add_argument('agent_key', type=str, help='Agent key to use when authenticating this application')
         parser.add_argument('portal_root', type=str, help='DukeDS portal url')
         parser.add_argument('openid_provider_id', type=str, help='OpenID provider from DukeDS api/v1/auth_providers')
 
     def handle(self, *args, **options):
-        url = options['url']
-        portal_root = options['portal_root']
-        openid_provider_id = options['openid_provider_id']
-        settings = DukeDSSettings.objects.first()
-        if settings:
-            settings.url = url
-            settings.portal_root = portal_root
-            settings.openid_provider_id = openid_provider_id
-            settings.save()
-        else:
-            DukeDSSettings.objects.create(url=url, portal_root=portal_root, openid_provider_id=openid_provider_id)
+        name = options['name']
+
+        try:
+            endpoint = DDSEndpoint.objects.get(name=name)
+            endpoint.update(**options)
+            endpoint.save()
+        except DDSEndpoint.DoesNotExist:
+            endpoint = DDSEndpoint.objects.create(**options)
