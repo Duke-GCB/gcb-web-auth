71b6e1f93d4d763087b8c5eea0564a93d2b32756
Dan Leehr <dan.leehr@duke.edu>
2018-06-05 14:22:35 -0400

    Install drf-jwt from setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gcb-web-auth',
-    version='0.10',
+    version='0.20',
     packages=find_packages(),
     install_requires=[
         'DukeDSClient==1.0.1',
         'PyJWT==1.5.2',
         'requests==2.18.1',
         'requests-oauthlib==0.8.0',
+        'djangorestframework-jwt==1.11.0',
     ],
     include_package_data=True,
     license='MIT License',
