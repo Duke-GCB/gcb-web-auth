f9def347476ed2f0c003bf815834d0659a904004
Dan Leehr <dan.leehr@duke.edu>
2018-06-07 11:52:37 -0400

    API view for session-authenticated clients to get JWT

--- gcb_web_auth/urls.py
+++ gcb_web_auth/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from jwt_views import session_jwt_token
 from . import views
 
 urlpatterns = [
@@ -13,5 +14,5 @@ urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
-
+    url(r'^api-token-session/', session_jwt_token),
 ]
