f35e25b8d54ee2e62c55ef6b7500b16a247f65e0
John Bradley <johnbradley2008@gmail.com>
2020-04-17 13:04:08 -0400

    create verison 1.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gcb-web-auth',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     install_requires=[
         'DukeDSClient==2.1.4',
