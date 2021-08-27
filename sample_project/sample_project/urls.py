337a712b1e12b9585072b1b9545cb79164a2fc16
John Bradley <johnbradley2008@gmail.com>
2021-08-27 13:33:50 -0400

    Upgrade to django 3.2.6
    Removes failing test_login_page. No need to test the sample project logic.

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -22,9 +22,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('gcb_web_auth.urls')),
-    url(r'^accounts/login/$', auth_views.login, {'template_name': 'gcb_web_auth/login.html'}, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, {'template_name': 'gcb_web_auth/logged_out.html'}, name='logout'),
-    url(r'^accounts/login-local/$', auth_views.login, {'template_name': 'gcb_web_auth/login-local.html'},
+    url(r'^accounts/login/$', auth_views.LoginView, {'template_name': 'gcb_web_auth/login.html'}, name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView, {'template_name': 'gcb_web_auth/logged_out.html'}, name='logout'),
+    url(r'^accounts/login-local/$', auth_views.LoginView, {'template_name': 'gcb_web_auth/login-local.html'},
         name='login-local'),
     # Redirect / to /accounts/login
     url(r'^$', RedirectView.as_view(pattern_name='auth-home', permanent=False)),
